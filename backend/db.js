d558d9842e9bdf48f7242396e436e1693e9b3a72
unknown <negivansh88@gmail.com>
2026-03-17 13:38:07 +0530

    db.js

--- backend/db.js
+++ backend/db.js
@@ -7,6 +7,9 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
+    ssl: process.env.NODE_ENV === 'production' ? {
+        rejectUnauthorized: false
+    } : false
 });
 
 const initializeDB = async () => {
