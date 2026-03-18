ee1f45708e518c9743682c845af2d9b133bb1d75
unknown <negivansh88@gmail.com>
2026-03-18 13:18:08 +0530

    logout

--- backend/index.js
+++ backend/index.js
@@ -12,18 +12,7 @@ const WebRTCSignaling = require('./socketServer');
 
 // ...existing code...
 
-// Middleware to verify JWT
-const authenticateToken = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-    if (token == null) return res.sendStatus(401);
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403);
-        req.user = user;
-        next();
-    });
-};
 
 // Logout a specific device/session (except current)
 app.post('/api/users/logout-session', authenticateToken, async (req, res) => {
